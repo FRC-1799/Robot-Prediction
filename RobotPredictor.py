8e93d6ae8723d93c3aa81f69955b9b9edc4c7a74
oof15642 <59772241+Fooot-Code@users.noreply.github.com>
2024-11-05 21:53:28 -0700

    Human Control Finished
    Slight error when going left or right only

--- RobotPredictor.py
+++ RobotPredictor.py
@@ -11,47 +11,34 @@ class RobotPredictor:
     def predict(self, location, otherRobotLocations, timeStep):
         self.location = location
         self.otherRobotLocations = otherRobotLocations
-
-        for i in range(1, len(self.otherRobotLocations)):
-            current = self.otherRobotLocations[i]
-            previous = self.otherRobotLocations[i-1]
-
-            # Add up velocity between each consecutive pair
-            slope = (current[1] - previous[1]) / (current[0] - previous[0])
-
-            yIntercept = current[1] / slope - current[0]
-
-            
-
-            
         
 
         # # How far ahead to predict the other robot's location (in frames)
-        # framesToPredict = self.fps * timeStep # timeStep is in seconds
+        framesToPredict = self.fps * timeStep # timeStep is in seconds
 
-        # if len(self.otherRobotLocations) >= 2:
-        #     # Calculate average velocity across all consecutive positions
-        #     total_velocity_x = 0
-        #     total_velocity_y = 0
-        #     num_velocities = 0
+        if len(self.otherRobotLocations) >= 2:
+            # Calculate average velocity across all consecutive positions
+            total_velocity_x = 0
+            total_velocity_y = 0
+            num_velocities = 0
             
-        #     for i in range(1, len(self.otherRobotLocations)):
-        #         current = self.otherRobotLocations[i]
-        #         previous = self.otherRobotLocations[i-1]
+            for i in range(1, len(self.otherRobotLocations)):
+                current = self.otherRobotLocations[i]
+                previous = self.otherRobotLocations[i-1]
                 
-        #         # Add up velocity between each consecutive pair
-        #         total_velocity_x += current[0] - previous[0] 
-        #         total_velocity_y += current[1] - previous[1]
-        #         num_velocities += 1
+                # Add up velocity between each consecutive pair
+                total_velocity_x += current[0] - previous[0] 
+                total_velocity_y += current[1] - previous[1]
+                num_velocities += 1
             
-        #     # Calculate average velocity
-        #     avg_velocity_x = total_velocity_x / num_velocities
-        #     avg_velocity_y = total_velocity_y / num_velocities
+            # Calculate average velocity
+            avg_velocity_x = total_velocity_x / num_velocities
+            avg_velocity_y = total_velocity_y / num_velocities
             
-        #     # Predict future position using the LAST position of the target robot
-        #     latest_pos = self.otherRobotLocations[-1]
-        #     predicted_x = latest_pos[0] + (avg_velocity_x * framesToPredict)
-        #     predicted_y = latest_pos[1] + (avg_velocity_y * framesToPredict)
+            # Predict future position using the LAST position of the target robot
+            latest_pos = self.otherRobotLocations[-1]
+            predicted_x = latest_pos[0] + (avg_velocity_x * framesToPredict)
+            predicted_y = latest_pos[1] + (avg_velocity_y * framesToPredict)
             return (predicted_x, predicted_y)
         
         return None  # Return None if we don't have enough positions to make a prediction
