496b580290ff562a08af204b0a655d1886ac49b4
oof15642 <59772241+Fooot-Code@users.noreply.github.com>
2025-01-03 14:17:31 -0700

    Documentation

--- RobotPredictor.py
+++ RobotPredictor.py
@@ -3,13 +3,23 @@ import numpy as np
 import math
 
 class RobotPredictor:
-    """Class that allows the prediction of robot positions based on their previous positions using numpy.polyfit and the second equation of motion"""
+    """
+    Class that allows the prediction of robot positions based on their previous positions using numpy.polyfit and the second equation of motion
+    """
     
     def __init__(self):
         self.robotToPredictPos = []  # list of tuples (x, y) of the robot that we want to predict's location
 
-    def able_to_predict(self, importedPositions):
-        """Determines if a robot's future positions are able to be predicted"""
+    def able_to_predict(self, importedPositions: list[tuple[int, int]]) -> bool:
+        """
+        Determines if a robot's future positions are able to be predicted
+
+        Parameters:
+        Imported Positions (list of tuples): Last positions that we have collected from the other robot
+
+        Returns:
+        bool: True if we are able to predict the robot, False if we cannot.
+        """
 
         self.robotToPredictPos = importedPositions
 
@@ -20,13 +30,26 @@ class RobotPredictor:
         else:
             return False
 
-    def distance_formula(self, x1, x2, y1, y2):
-        """Calculates the distance between 4 points in a 2 dimensional plane"""
+    def distance_formula(self, x1: int, x2: int, y1: int, y2: int) -> float:
+        """
+        Calculates the distance between 2 points in a 2 dimensional plane
+        
+        Parameters:
+        x1, x2, y1, y2 (ints): X and Y position of the robot's last position vs. the X and Y position that they are at now
+
+        Returns:
+        float: The distance (hypotenuse length) between the 2 points
+        """
 
         return math.sqrt((x2-x1)**2 + (y2-y1)**2)
     
-    def find_x_position(self):
-        """Uses the second equation of motion to predict the future X position of the robot"""
+    def find_x_position(self) -> float:
+        """
+        Uses the second equation of motion to predict the future X position of the robot
+
+        Returns:
+        float: Decimal value of the X position based on the previous X positions
+        """
 
         distanceBetweenTheTwoVariables = self.distance_formula(self.lastXPosition, self.currentXPosition, self.lastYPosition, self.currentYPosition)
         divisor = self.timePassed / self.timeStep # only used to calculate the predictedXPostition
@@ -42,8 +65,18 @@ class RobotPredictor:
 
         return predictedYPosition
 
-    def predict(self, otherRobotLocations, timeStep, timePassed):
-        """Predicts the robot's future locations based on its previous locations"""
+    def predict(self, otherRobotLocations: list[tuple[float, float]], timeStep: float, timePassed: float) -> tuple[list[float], tuple[float, float]]:
+        """
+        Predicts the robot's future locations based on its previous locations
+        
+        Parameters:
+        otherRobotLocations (list[tuple[float, float]]): list of the robot's previous locations. For example [(24.3, 54.3), (48.6, 108.6)]
+        timeStep (float): How far ahead you want the prediction to be
+        timePassed (float): How much time has passed since the last prediction
+
+        Returns:
+        tuple[list[float], tuple[float, float]]: Tuple containing the coefficients of the parabola and the predicted x and y locations based on that parabola
+        """
 
         self.timeStep = timeStep
         self.timePassed = timePassed
@@ -68,6 +101,3 @@ class RobotPredictor:
         predictedPosition = (self.predictedXPosition, self.predictedYPosition)
 
         return (self.coefficients, predictedPosition)
-
-    def return_xy_values(self):
-        return (self.robotToPredictX, self.robotToPredictY)
